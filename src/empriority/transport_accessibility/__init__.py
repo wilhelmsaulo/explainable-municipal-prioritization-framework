e14027835e17090bfddb6504f70d5a719cffd13c
wilhelmsaulo <saulo.costa@ifpa.edu.br>
2026-08-06 11:43:15 -0300

    Initialize transport accessibility module

---
+++ src/empriority/transport_accessibility/__init__.py
@@ -0,0 +1,5 @@
+"""Multimodal transport accessibility using official Brazilian transport data."""
+
+from .catalog import build_transport_source_catalog
+
+__all__ = ["build_transport_source_catalog"]
